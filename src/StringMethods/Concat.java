587a6e0ed5c966d1805e7f70d35fe8b05196a131
saikiranbetha <saikiranbetha.vtalent@gmail.com>
2022-09-08 19:06:45 +0530

    Concat

--- src/StringMethods/Concat.java
+++ src/StringMethods/Concat.java
@@ -4,6 +4,15 @@ public class Concat {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String FirstName = "Sai ";
+		String MiddleName = "Kiran ";
+		
+		String result = FirstName.concat(MiddleName);
+		System.out.println(result);
+		
+		String LastName = "Betha";
+		String result1 = result.concat(LastName);
+		System.out.println(result1);
 
 	}
 
