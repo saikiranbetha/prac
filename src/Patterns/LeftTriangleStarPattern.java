aac5cfb8204945cf4a2f700ee4d768c7f0967725
saikiranbetha <saikiranbetha.vtalent@gmail.com>
2022-09-09 16:15:26 +0530

    pattern

--- src/Patterns/LeftTriangleStarPattern.java
+++ src/Patterns/LeftTriangleStarPattern.java
@@ -3,7 +3,7 @@ package Patterns;
 public class LeftTriangleStarPattern {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		int i, j, row = 7;
 		for (i=0; i<row; i++) {
 			for (j=2*(row-(i+1)); j>=1; j--) {
