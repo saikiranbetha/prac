516d0236c3b6d557e9e9bb828fa4bd7a2d86b1d5
saikiranbetha <saikiranbetha.vtalent@gmail.com>
2022-09-29 22:54:53 +0530

    Anagram Code

---
+++ src/ProgramsOnStrings/Anagram.java
@@ -0,0 +1,92 @@
+package ProgramsOnStrings;
+
+import java.util.Scanner;
+
+public class Anagram {
+	
+	static String removeSpaces(String str) {
+		char[] ch=str.toCharArray();
+		
+		String nstr = "";
+		
+		for (int i=0; i<ch.length; i++) {
+			if(ch[i]!=' ') 
+				nstr = nstr + ch[i];
+		}
+		return nstr;
+	}
+	
+	
+	
+	static String toLowerCase(String str) {
+		char[] ch = str.toCharArray();
+		
+		String nstr = "";
+		
+		for (int i=0; i < ch.length; i++) {
+			if(ch[i] >= 65 && ch[i] <= 90) {
+				nstr = nstr + ((char)ch[i] + 32);
+			}
+			else {
+				nstr = nstr + ch[i];
+			}
+				
+		}
+		return nstr;
+	}
+	
+	
+	static String sort(String str){
+		char[]ch=str.toCharArray();
+			
+		for(int i=0; i<ch.length-1; i++){
+			for(int j=i+1; j<ch.length; j++){
+				if(ch[i]>ch[j]){
+					char t=ch[i];
+					ch[i]=ch[j];
+					ch[j]=t;
+				}
+			}
+		}
+		String st= new String(ch);
+		return st;
+	}
+	static boolean compare(String s1,String s2){
+		if(s1.length() != s2.length())
+			return false;
+		else{
+			s1 = toLowerCase(s1);
+			s2 = toLowerCase(s2);
+			s1 = sort(s1);
+			s2 = sort(s2);
+			char ch1[] = s1.toCharArray();
+			char ch2[] = s2.toCharArray();
+		for(int i=0; i<ch1.length; i++){
+			if(ch1[i]!=ch2[i]){
+				return false;
+				}
+			}
+		return true;
+		}
+	}
+	
+	
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first string");
+		String s1 = sc.nextLine();
+		System.out.println("Enter the second string");
+		String s2 = sc.nextLine();
+		s1 = removeSpaces(s1);
+		s2 = removeSpaces(s2);
+		boolean b = compare(s1,s2);
+		
+		if(b)
+			System.out.println("String is Anagram");
+		else
+			System.out.println("Not an Anagram");
+
+	}
+
+}
